c260fa8c0effa802c20c646f8942becb7d99dfa4
VISHALS566 <viahalvishal36@gmail.com>
2025-12-06 11:27:58 +0530

    Added production CSS

---
+++ tailwind.congig.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html"], // <--- This tells it to look at your HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
